f506610caf50c6851ea5f6b85fca8fe8eb952c25
Nathaniel Grenon <Nathaniel.Grenon@hotmail.com>
2020-03-06 16:30:23 -0500

    Add to my file

---
+++ _patterns/header/header.js
@@ -0,0 +1,5 @@
+var btn = document.querySelector(".nav-icon");
+btn.addEventListener("click", function myFunction() {
+  var x = document.getElementById("myTopnav");
+  x.classList.toggle("responsive");
+})
